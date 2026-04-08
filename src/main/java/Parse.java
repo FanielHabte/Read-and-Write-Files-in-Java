ecc899394ced6ae63fff376c1c4e4a72a02fbade
Fani <fanielhabte20@gmail.com>
2026-04-07 18:31:09 -0700

    Created Json recrusive parser

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -1,8 +1,14 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Parse {
     BufferedReader bufferedReader;
@@ -11,12 +17,12 @@ public class Parse {
     HashMap <Integer, ArrayList<Object>> dataSet = new HashMap<>();
     int rowNumber = 0;
 
-    Parse(BufferedReader bufferedReader) {
+    public Parse(BufferedReader bufferedReader) {
         this.bufferedReader = bufferedReader;
     }
 
     // Complete
-    void textFile () throws IOException {
+    public void textFile () throws IOException {
         while ((fileLine = bufferedReader.readLine()) != null) {
             String [] values = fileLine.split("\\|");
             rowValues = new ArrayList<>(Arrays.asList(values));
@@ -28,16 +34,35 @@ public class Parse {
         System.out.println(dataSet);
     }
 
-    void jsonFile () throws IOException {
-        while ((fileLine = bufferedReader.readLine()) != null) {
-            String [] values = fileLine.split("\\|");
-            for (String value: values) {
-                System.out.println(value);
+    // Completed
+    public void jsonFile () throws IOException {
+        Gson gson = new Gson();
+        JsonObject sampleOrders = gson.fromJson(bufferedReader, JsonObject.class);
+        JsonArray orders = sampleOrders.getAsJsonArray("orders");
+        HashMap<Integer, HashMap<String, Object>> dataset = new HashMap<>();
+        for (JsonElement order: orders) {
+            HashMap<String, Object> rows = new HashMap<>();
+            rowNumber++;
+            flatten(order, rows);
+            dataset.put(rowNumber,rows);
+        }
+        System.out.println(dataset);
+    }
+
+    public void flatten (JsonElement order, HashMap<String, Object> rows) {
+        for (Map.Entry<String, JsonElement> entry : order.getAsJsonObject().entrySet()) {
+            String key = entry.getKey();
+            JsonElement value = entry.getValue();
+            if (value.isJsonObject()) {
+                flatten(value.getAsJsonObject(), rows);
+            } else {
+                rows.put(key, value.getAsString());
             }
         }
     }
 
-    void parquetFile () throws IOException {
+    // Not started
+    public void parquetFile () throws IOException {
         while ((fileLine = bufferedReader.readLine()) != null) {
             String [] values = fileLine.split("\\|");
             for (String value: values) {
@@ -47,7 +72,7 @@ public class Parse {
     }
 
     // Complete
-    void csvFile () throws IOException {
+    public void csvFile () throws IOException {
         while ((fileLine = bufferedReader.readLine()) != null) {
             Object [] stringValues = fileLine.split(",");
             rowValues = new ArrayList<>(Arrays.asList(stringValues));
@@ -59,7 +84,8 @@ public class Parse {
         System.out.println(dataSet);
     }
 
-    void htmlFile () throws IOException {
+    // Not Started
+    public void htmlFile () throws IOException {
         while ((fileLine = bufferedReader.readLine()) != null) {
             String [] values = fileLine.split("\\|");
             for (String value: values) {
@@ -68,7 +94,8 @@ public class Parse {
         }
     }
 
-    void xmlFile () throws IOException {
+    // Not Started
+    public void xmlFile () throws IOException {
         while ((fileLine = bufferedReader.readLine()) != null) {
             String [] values = fileLine.split("\\|");
             for (String value: values) {
